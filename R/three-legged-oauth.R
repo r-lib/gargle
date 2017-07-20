08d0e3b0a0e3c64b49660515a9e305af1f34c435
jennybc <jenny@stat.ubc.ca>
2017-07-20 16:26:02 -0700

    Put tidyverse app into its own function

--- R/three-legged-oauth.R
+++ R/three-legged-oauth.R
@@ -1,21 +1,18 @@
 
 #' Create a token for a user via the browser flow.
-#' @param scopes List of scopes required for the returned token.
-#' @param oauth_app `httr::oauth_app` to use for this token fetch. (Optional.)
+#' @inheritParams httr::oauth2.0_token
 #' @param ... Additional arguments (ignored)
 #' @export
-credentials_user_oauth2 <- function(scopes, oauth_app = NULL, ...) {
-
-  app <- httr::oauth_app(
-    "google",
-    key = "603366585132-nku3fbd298ma3925l12o2hq0cc1v8u11.apps.googleusercontent.com",
-    secret = "as_N12yfWLRL9RMz5nVpgCZt"
-  )
+credentials_user_oauth2 <- function(scopes,
+                                    app,
+                                    cache = getOption("httr_oauth_cache"),
+                                    ...) {
   token <- httr::oauth2.0_token(
     endpoint = httr::oauth_endpoints("google"),
     app = app,
     scope = scopes,
-    use_oob = FALSE
+    use_oob = FALSE,
+    cache = cache
   )
   token
 }
---
+++ R/tidyverse-oauth-app.R
@@ -0,0 +1,7 @@
+tidyverse_app <- function() {
+  httr::oauth_app(
+    appname = "tidyverse",
+    key = "603366585132-nku3fbd298ma3925l12o2hq0cc1v8u11.apps.googleusercontent.com",
+    secret = "as_N12yfWLRL9RMz5nVpgCZt"
+  )
+}
