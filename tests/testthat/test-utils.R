18251cd66717ff4b66cc929072d74225b5fb84db
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-11-10 22:32:56 -0800

    Add test for add_email_scope()

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -6,3 +6,16 @@ test_that("%||% works", {
   expect_equal(1, c() %||% 1)
   expect_equal(1, 1 %||% stop("oh no"))
 })
+
+test_that("add_email_scope() works", {
+  expect_identical(add_email_scope(), "email")
+  expect_identical(add_email_scope("email"), "email")
+  expect_identical(
+    add_email_scope("whatever"),
+    c("whatever", "email")
+  )
+  expect_equivalent(
+    add_email_scope(c("whatever" = "whatever")),
+    c("whatever", "email")
+  )
+})
