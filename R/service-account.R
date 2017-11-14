43eb63c2fab4b58e4ac50aa1db986af4fc81c851
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-11-14 14:09:55 -0800

    Remove TODOs re: service tokens

--- R/service-account.R
+++ R/service-account.R
@@ -1,4 +1,3 @@
-
 #' Create a token for a Google service account.
 #'
 #' @param scopes List of scopes required for the returned token.
@@ -15,15 +14,12 @@ credentials_service_account <- function(scopes, path = "", ...) {
   token <- httr::TokenServiceAccount$new(
     endpoint = NULL,
     secrets = info,
-    ## TODO(jennybc) Is is really true I can't add the "email" scope here?
     params = list(scope = scopes)
   )
   if (is.null(token$credentials$access_token) ||
       !nzchar(token$credentials$access_token)) {
     NULL
   } else {
-    ## TODO(jennybc) this needs to be baked into the sub-classed service token
-    ## object, once such exists
     message("email: ", info[["client_email"]])
     token
   }
