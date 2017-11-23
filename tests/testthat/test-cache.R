a82f9d693707ad22c511129c4a524332d8f53838
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-11-23 11:46:19 -0800

    Tests re: retrieving tokens from cache

--- tests/testthat/test-cache.R
+++ tests/testthat/test-cache.R
@@ -28,3 +28,67 @@ test_that("token_upsert() updates token already in cache", {
   i <- which(cache$email == "b")
   expect_identical(cache$token[[i]][["credentials"]], list(a = 2))
 })
+
+test_that("token_match() retrieves an exact hash match", {
+  tc <- tempfile("cache-")
+
+  fauxen_a <- gargle2.0_token(
+    email = "a",
+    scope = "a",
+    credentials = list(a = 1),
+    cache = tc
+  )
+  fauxen_b <- gargle2.0_token(email = "a", scope = "a", cache = tc)
+  expect_equal(fauxen_a, fauxen_b)
+})
+
+test_that("token_match() returns a single multi match", {
+  tc <- tempfile("cache-")
+
+  fauxen_a <- gargle2.0_token(
+    email = "a",
+    scope = "a",
+    credentials = list(a = 1),
+    cache = tc
+  )
+  fauxen_b <- gargle2.0_token(scope = "a", cache = tc)
+  expect_equal(fauxen_a, fauxen_b)
+})
+
+test_that("token_match() fails for >1 multi match, if non-interactive", {
+  tc <- tempfile("cache-")
+
+  fauxen_a <- gargle2.0_token(
+    email = "a",
+    scope = "a",
+    credentials = list(a = 1),
+    cache = tc
+  )
+  fauxen_b <- gargle2.0_token(
+    email = "b",
+    scope = "a",
+    credentials = list(b = 1),
+    cache = tc
+  )
+  expect_error(
+    gargle2.0_token(scope = "a", cache = tc),
+    "Multiple cached tokens"
+  )
+})
+
+test_that("token_match() works with normalized scopes, uses 'set' mentality ", {
+  tc <- tempfile("cache-")
+
+  fauxen_a <- gargle2.0_token(
+    email = "a",
+    scope = c("a", "b", "c"),
+    credentials = list(a = 1),
+    cache = tc
+  )
+
+  fauxen_b <- gargle2.0_token(scope = "a", cache = tc)
+  expect_equal(fauxen_a, fauxen_b)
+
+  fauxen_c <- gargle2.0_token(scope = c("c", "a", "b"), cache = tc)
+  expect_equal(fauxen_a, fauxen_c)
+})
