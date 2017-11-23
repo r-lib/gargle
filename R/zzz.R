a09eccae42b719bae9d8dfa6c09758efca98463d
Jenny Bryan <jenny.f.bryan@gmail.com>
2017-11-22 21:53:49 -0800

    Use backports

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,8 @@
 .onLoad <- function(lib, pkg) {
   cred_funs_set_default()
   debugme::debugme()
+
+  backports::import(pkg, c("dir.exists", "file.size", "startsWith", "endsWith"))
+
+  invisible()
 }
