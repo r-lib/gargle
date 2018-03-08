33028b10b05c13488d95b321d68e1ca3a77c08f4
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-03-07 23:11:07 -0800

    "user_agent" instead of "ua"

--- R/make-request.R
+++ R/make-request.R
@@ -8,12 +8,13 @@
 #' @param x List. Holding the components for an HTTP request, presumably created
 #'   with [`generate_request()`] or [build_request()]. Must contain the a
 #'   `method` and `url`. If present, `body` and `token` are used.
+#' @param user_agent A user agent string, prepared by [httr::user_agent()].
 #' @param ... Optional arguments passed through to the HTTP method.
 #'
 #' @return Object of class `response` from [httr].
 #' @export
 #' @family requests and responses
-make_request <- function(x, ..., ua = gargle_ua()) {
+make_request <- function(x, ..., user_agent = gargle_user_agent()) {
   method <- list(
     "GET" = httr::GET,
     "POST" = httr::POST,
@@ -25,12 +26,12 @@ make_request <- function(x, ..., ua = gargle_ua()) {
     url = x$url,
     body = x$body,
     x$token,
-    ua,
+    user_agent,
     ...
   )
 }
 
-gargle_ua <- function() {
+gargle_user_agent <- function() {
   httr::user_agent(paste0(
     "gargle/", utils::packageVersion("gargle"), " ",
     "httr/", utils::packageVersion("httr")
