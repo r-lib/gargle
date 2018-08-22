0c8fcb4d356a890c10213d132092b9a738b43def
Jenny Bryan <jenny.f.bryan@gmail.com>
2018-08-21 23:04:42 -0700

    Refactor cache

--- R/utils.R
+++ R/utils.R
@@ -2,11 +2,117 @@
   if (length(a) > 0) a else b
 }
 
-#' @importFrom glue glue glue_data collapse
+#' @importFrom glue glue glue_data glue_collapse
 #' @importFrom magrittr %>%
 NULL
 
 is_string <- function(x) is.character(x) && length(x) == 1
+
+commapse <- function(...) paste0(..., collapse = ", ")
+
+cat_line <- function(...) cat(paste0(..., "\n"), sep = "")
+
+bt <- function(x) encodeString(x, quote = "`")
+
+stop_glue <- function(..., .sep = "", .envir = parent.frame(),
+                      call. = FALSE, .domain = NULL) {
+  stop(
+    glue(..., .sep = .sep, .envir = .envir),
+    call. = call., domain = .domain
+  )
+}
+
+stop_glue_data <- function(..., .sep = "", .envir = parent.frame(),
+                           call. = FALSE, .domain = NULL) {
+  stop(
+    glue_data(..., .sep = .sep, .envir = .envir),
+    call. = call., domain = .domain
+  )
+}
+
+stop_collapse <- function(x) {
+  stop(glue_collapse(x, sep = "\n"), call. = FALSE)
+}
+
+warning_glue <- function(..., .sep = "", .envir = parent.frame(),
+                         call. = FALSE, .domain = NULL) {
+  warning(
+    glue(..., .sep = .sep, .envir = .envir),
+    call. = call., domain = .domain
+  )
+}
+
+warning_glue_data <- function(..., .sep = "", .envir = parent.frame(),
+                              call. = FALSE, .domain = NULL) {
+  warning(
+    glue_data(..., .sep = .sep, .envir = .envir),
+    call. = call., domain = .domain
+  )
+}
+
+warning_collapse <- function(x) {
+  warning(glue_collapse(x, sep = "\n"))
+}
+
+message_glue <- function(..., .sep = "", .envir = parent.frame(),
+                         .domain = NULL, .appendLF = TRUE) {
+  message(
+    glue(..., .sep = .sep, .envir = .envir),
+    domain = .domain, appendLF = .appendLF
+  )
+}
+
+message_glue_data <- function(..., .sep = "", .envir = parent.frame(),
+                              .domain = NULL) {
+  message(
+    glue_data(..., .sep = .sep, .envir = .envir),
+    domain = .domain
+  )
+}
+
+message_collapse <- function(x) {
+  message(glue_collapse(x, sep = "\n"))
+}
+
+isFALSE <- function(x) identical(x, FALSE)
+
+is.oauth_app <- function(x) inherits(x, "oauth_app")
+
+is.oauth_endpoint <- function(x) inherits(x, "oauth_endpoint")
+
+add_line <- function(path, line, quiet = FALSE) {
+  if (file.exists(path)) {
+    lines <- readLines(path, warn = FALSE)
+    lines <- lines[lines != ""]
+  } else {
+    lines <- character()
+  }
+
+  if (line %in% lines) return(TRUE)
+  if (!quiet) message("Adding ", line, " to ", path)
+
+  lines <- c(lines, line)
+  writeLines(lines, path)
+  TRUE
+}
+
+rhash <- function(obj) {
+  # first 14 bytes are serialization header
+  # skip them to allow hash comparison "across platforms and some R versions"
+  # see digest::digest() help and source for more background
+  msg <- serialize(obj, connection = NULL, ascii = FALSE)[-(1:14)]
+  paste(openssl::md5(msg), collapse = "")
+}
+
+is_testing <- function() {
+  identical(Sys.getenv("TESTTHAT"), "true")
+}
+
+## in the spirit of basename(), but for Google scopes
+## for printing purposes
+base_scope <- function(x) {
+  gsub("/$", "", gsub("(.*)/(.+$)", "...\\2", x))
+}
 
 #' An expose object
 #'
@@ -40,63 +146,3 @@ expose <- function() structure(list(), class = "expose")
 #' @export
 #' @rdname expose
 is_expose <- function(x) inherits(x, "expose")
-
-commapse <- function(...) paste0(..., collapse = ", ")
-
-cat_line <- function(...) cat(paste0(..., "\n"), sep = "")
-
-bt <- function(x) encodeString(x, quote = "`")
-
-stop_glue <- function(..., .sep = "", .envir = parent.frame(),
-                      call. = FALSE, .domain = NULL) {
-  stop(
-    glue(..., .sep = .sep, .envir = .envir),
-    call. = call., domain = .domain
-  )
-}
-
-stop_glue_data <- function(..., .sep = "", .envir = parent.frame(),
-                           call. = FALSE, .domain = NULL) {
-  stop(
-    glue_data(..., .sep = .sep, .envir = .envir),
-    call. = call., domain = .domain
-  )
-}
-
-stop_collapse <- function(x) stop(collapse(x, sep = "\n"), call. = FALSE)
-
-warning_glue <- function(..., .sep = "", .envir = parent.frame(),
-                         call. = FALSE, .domain = NULL) {
-  warning(
-    glue(..., .sep = .sep, .envir = .envir),
-    call. = call., domain = .domain
-  )
-}
-
-warning_glue_data <- function(..., .sep = "", .envir = parent.frame(),
-                              call. = FALSE, .domain = NULL) {
-  warning(
-    glue_data(..., .sep = .sep, .envir = .envir),
-    call. = call., domain = .domain
-  )
-}
-
-warning_collapse <- function(x) warning(collapse(x, sep = "\n"))
-
-message_glue <- function(..., .sep = "", .envir = parent.frame(),
-                         .domain = NULL, .appendLF = TRUE) {
-  message(
-    glue(..., .sep = .sep, .envir = .envir),
-    domain = .domain, appendLF = .appendLF
-  )
-}
-
-message_glue_data <- function(..., .sep = "", .envir = parent.frame(),
-                              .domain = NULL) {
-  message(
-    glue_data(..., .sep = .sep, .envir = .envir),
-    domain = .domain
-  )
-}
-
-message_collapse <- function(x) message(collapse(x, sep = "\n"))
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -19,3 +19,24 @@ test_that("add_email_scope() works", {
     c("whatever", "email")
   )
 })
+
+test_that("rhash() produces same hash across platforms and R versions", {
+ expect_identical(rhash(iris), "d3c5d071001b61a9f6131d3004fd0988")
+})
+
+test_that("base_scope() extracts the last scope part", {
+  scopes <- c(
+    "email",
+    "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.modify",
+    "https://www.googleapis.com/auth/gmail.compose",
+    "https://mail.google.com/"
+  )
+  out <- base_scope(scopes)
+  expect_identical(
+    out,
+    c("email", "...drive", "...gmail.readonly", "...gmail.modify",
+      "...gmail.compose", "...mail.google.com")
+  )
+})
