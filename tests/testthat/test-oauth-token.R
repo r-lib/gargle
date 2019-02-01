c484d7170a5ada61a386e6fb868c72cf6e83b972
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-02-01 11:15:02 -0800

    Test email ingest

---
+++ tests/testthat/test-oauth-token.R
@@ -0,0 +1,27 @@
+context("Gargle2.0 oauth tokens")
+
+test_that("email is ingested correctly", {
+  fauxen_email <- function(email = NULL) {
+    gargle2.0_token(email = email, credentials = list(a = 1))$email
+  }
+  expect_null(fauxen_email())
+  expect_null(fauxen_email(NULL))
+  expect_null(fauxen_email(NA))
+  expect_null(fauxen_email(FALSE))
+  expect_identical(fauxen_email(TRUE), "*")
+  expect_identical(fauxen_email("a@example.org"), "a@example.org")
+})
+
+test_that("email can be set in option", {
+  fauxen_email <- function(email = NULL) {
+    withr::with_options(
+      list(gargle.oauth_email = email),
+      gargle2.0_token(credentials = list(a = 1))$email
+    )
+  }
+  expect_null(fauxen_email(NULL))
+  expect_null(fauxen_email(NA))
+  expect_null(fauxen_email(FALSE))
+  expect_identical(fauxen_email(TRUE), "*")
+  expect_identical(fauxen_email("a@example.org"), "a@example.org")
+})
