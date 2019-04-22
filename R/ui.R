a2aac688d1cbf1fa261f27c2445c37abe025d27e
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-04-22 14:57:27 -0700

    Move more ui functions out of utils

--- R/ui.R
+++ R/ui.R
@@ -8,3 +8,33 @@ cat_line <- function(..., quiet = getOption("gargle_quiet", default = TRUE)) {
   lines <- paste0(..., "\n")
   cat(lines, sep = "")
 }
+
+cat_glue <- function(..., .sep = "", .envir = parent.frame()) {
+  cat(glue(..., .sep = .sep, .envir = .envir), sep = "\n")
+}
+
+cat_glue_data <- function(..., .sep = "", .envir = parent.frame()) {
+  cat(glue_data(..., .sep = .sep, .envir = .envir), sep = "\n")
+}
+
+commapse <- function(...) paste0(..., collapse = ", ")
+bt <- function(x) encodeString(x, quote = "`")
+sq <- function(x) encodeString(x, quote = "'")
+
+## obscure the middle bit of (sensitive?) strings with '...'
+## obfuscate("sensitive", first = 3, last = 2) = "sen...ve"
+obfuscate <- function(x, first = 6, last = 4) {
+  nc <- nchar(x)
+  ellipsize <- nc > first + last
+  out <- x
+  out[ellipsize] <-
+    paste0(
+      substr(x[ellipsize], start = 1, stop = first),
+      "...",
+      substr(x[ellipsize],
+             start = nc[ellipsize] - last + 1,
+             stop = nc[ellipsize]
+      )
+    )
+  out
+}
