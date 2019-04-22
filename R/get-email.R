22ba20621e2393e109fcf94c8e26ac3d4608cd24
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-04-22 10:02:05 -0700

    Prioritize get_userinfo, improve internal docs

--- R/get-email.R
+++ R/get-email.R
@@ -1,4 +1,4 @@
-# Good overview re: learning about your Google user / token:
+# Good 3rd party overview re: learning about your Google user / token:
 # https://www.oauth.com/oauth2-servers/signing-in-with-google/verifying-the-user-info/
 #
 # It suggests perhaps we could recover, e.g., the email during token initiation?
@@ -11,26 +11,55 @@
 # Both require an additional HTTP request, so are "not recommended for
 # production applications".
 #
+# I had working code to access both the userinfo and tokeninfo endpoints before
+# I read this and there are a few differences between what I do and what they
+# say to do. The discrepancies are around which token to send (ID? access? etc.)
+# and where/how to send it. I conclude that there must be a few variations that
+# work.
+#
 # Google's docs re: how to get user info:
 # https://developers.google.com/identity/protocols/OpenIDConnect#obtaininguserprofileinformation
 # "Add your access token to the authorization header and make an HTTPS GET
 # request to the userinfo endpoint, which you should retrieve from the Discovery
 # document using the key userinfo_endpoint."
-
+#
 # Here's the OpenID Connect discovery document:
 # https://accounts.google.com/.well-known/openid-configuration
 #
 # Here's the URL for userinfo_endpoint, at the time of writing:
 # https://openidconnect.googleapis.com/v1/userinfo
-#
-# I had working code to access both of these endpoints before I read this and
-# there are a few differences between what I do and what they say to do. The
-# discrepancies are around which token to send (ID? access? etc.) and where/how
-# to send it. I conclude that there must be a few variations that work.
 
 
-# Note that gargle only includes the userinfo.email scope, as a global default,
-# so we don't expect to get back general profile info.
+#' Get info on current user
+#'
+#' The userinfo endpoint can potentially return the user's entire profile, if
+#' the token has the necessary scope. But, by default, gargle-mediated tokens
+#' only include the `userinfo.email` scope, so don't expect to get much beyond
+#' the email, unless you've explicitly scoped for more. `get_userinfo()` can
+#' exploit built-in token refresh, although at the time of writing, the
+#' motivating use case is to get and store the email for a freshly-obtained
+#' token.
+#'
+#' @param token A gargle token
+#'
+#' @return User info or user's email
+#' @keywords internal
+#' @examples
+#' \dontrun{
+#' # with service account token
+#' t <- token_fetch(
+#'   scope = "https://www.googleapis.com/auth/drive",
+#'   path = "path/to/service/account/token/blah-blah-blah.json"
+#' )
+#' # or with an OAuth token
+#' t <- token_fetch(
+#'   scope = "https://www.googleapis.com/auth/drive",
+#'   email = "janedoe@example.com"
+#' )
+#' get_userinfo(t)
+#' get_email(t)
+#' get_tokeninfo(t)
+#' }
 get_userinfo <- function(token) {
   stopifnot(inherits(token, "Token2.0"))
 
@@ -44,12 +73,21 @@ get_userinfo <- function(token) {
   ## TODO(jennybc) use process_response() once it's available in same pkg
   httr::stop_for_status(raw_resp)
   res <- httr::content(raw_resp, as = "parsed", type = "application/json")
-  #res$email
   res
 }
 
-# WARNING: does not validate or refresh token!
-# Use when you expect the token to be valid for external reasons.
+#' @rdname get_userinfo
+# Assumes the token was obtained with userinfo.email scope.
+# This is true of all gargle-mediated tokens, by definition.
+get_email <- function(token) {
+  get_userinfo(token)$email
+}
+
+
+# WARNING: does not trigger token refresh!
+# A token that needs to be refreshed can result in "Bad Request (HTTP 400)."
+# Use when you expect the token to be valid for external reasons, e.g. it is
+# known to be fresh.
 get_tokeninfo <- function(token) {
   stopifnot(inherits(token, "Token2.0"))
 
@@ -67,10 +105,4 @@ get_tokeninfo <- function(token) {
   httr::stop_for_status(raw_resp)
   res <- httr::content(raw_resp, as = "parsed", type = "application/json")
   res
-}
-
-# Assumes the token was obtained with userinfo.email scope.
-# This will be true of all gargle-mediated tokens, by definition.
-get_email <- function(token) {
-  get_tokeninfo(token)$email
 }
