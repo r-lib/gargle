971c4345568bece76edbaf14acbc3c48834841c9
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-05-03 15:15:03 -0700

    Return more data from stop_bad_class()

--- R/conditions.R
+++ R/conditions.R
@@ -8,11 +8,15 @@ stop_glue <- function(..., .sep = "", .envir = parent.frame(),
 
 stop_bad_class <- function(object, expected_class) {
   nm <- rlang::as_name(rlang::ensym(object))
-  actual <- glue_collapse(class(object), sep = "/")
+  actual_class <- class(object)
+  actual <- glue_collapse(actual_class, sep = "/")
   expected <- glue_collapse(expected_class, sep = ", ", last = " or ")
   message <- glue("{bt(nm)} must be {expected}, not of class {sq(actual)}.")
   abort(
     "gargle_error_bad_class",
-    message = message
+    message = message,
+    object_name = nm,
+    actual_class = actual_class,
+    expected_class = expected_class
   )
 }
