700f771107d2ed0bbd87ed92a53226b2f7442970
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-06-05 12:29:14 -0700

    Resurrect tests of API keys

--- tests/testthat/test-inside-the-house.R
+++ tests/testthat/test-inside-the-house.R
@@ -10,3 +10,33 @@ test_that("it is possible to be 'outside the house'", {
     "restricted to specific tidyverse packages"
   )
 })
+
+test_that("tidyverse API key", {
+  key <- tidyverse_api_key()
+  expect_true(is_string(key))
+  expect_error(
+    local(gargle:::tidyverse_api_key(), envir = globalenv()),
+    "restricted"
+  )
+})
+
+test_that("gargle API key", {
+  key <- gargle_api_key()
+  expect_true(is_string(key))
+  expect_error(
+    local(gargle:::gargle_api_key(), envir = globalenv()),
+    "restricted"
+  )
+})
+
+# test_that("tidyverse oauth app", {
+#   oa <- tidyverse_app()
+#   expect_s3_class(oa, "oauth_app")
+#   expect_identical(oa$appname, "tidyverse")
+# })
+#
+# test_that("gargle oauth app", {
+#   oa <- gargle_app()
+#   expect_s3_class(oa, "oauth_app")
+#   expect_identical(oa$appname, "gargle-demo")
+# })
