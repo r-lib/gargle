28d44600bdfce2d90819c1066d57f640f840e1d5
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-06-04 21:09:30 -0700

    Add and test check_*()

--- R/inside-the-house.R
+++ R/inside-the-house.R
@@ -8,3 +8,15 @@ from_permitted_package <- function(env = parent.frame()) {
   cat_line("attempt from: ", nm)
   nm %in% c("gargle", "googledrive", "googlesheets4", "gmailr", "bigrquery")
 }
+
+check_permitted_package <- function(env = parent.frame()) {
+  if (!from_permitted_package(env)) {
+    msg <- paste(
+      "Resource is only available inside specific tidyverse packages.",
+      "Do you need to create your own OAuth app or API key?",
+      sep = "\n"
+    )
+    abort(msg)
+  }
+  invisible(env)
+}
--- tests/testthat/test-inside-the-house.R
+++ tests/testthat/test-inside-the-house.R
@@ -1,7 +1,12 @@
 test_that("gargle is 'inside the house'", {
   expect_true(from_permitted_package())
+  expect_error_free(check_permitted_package())
 })
 
-test_that("from_permitted_package() can return FALSE", {
+test_that("it is possible to be 'outside the house'", {
   expect_false(local(gargle:::from_permitted_package(), envir = globalenv()))
+  expect_error(
+    local(gargle:::check_permitted_package(), envir = globalenv()),
+    "only.*tidyverse"
+  )
 })
