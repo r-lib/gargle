37b9415eca770a090b8a4f94db3f542d799a35df
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-10-02 13:36:12 -0700

    Explicitly do NOT cache these tokens
    Without setting `cache_path = FALSE`, running `devtools::test()` in the Console causes undesirable user interaction re: "Use a local file ('.httr-oauth'), to cache OAuth access credentials between R sessions?"

--- tests/testthat/test-credentials-byo-auth2.R
+++ tests/testthat/test-credentials-byo-auth2.R
@@ -10,7 +10,8 @@ test_that("credentials_byo_auth2() rejects a token that obviously not Google", {
   token <- httr::Token2.0$new(
     app = httr::oauth_app("x", "y", "z"),
     endpoint = httr::oauth_endpoints("github"),
-    credentials = list(access_token = "ACCESS_TOKEN")
+    credentials = list(access_token = "ACCESS_TOKEN"),
+    cache_path = FALSE
   )
 
   expect_error(
@@ -23,7 +24,8 @@ test_that("credentials_byo_auth2() just passes valid input through", {
   token <- httr::Token2.0$new(
     app = httr::oauth_app("x", "y", "z"),
     endpoint = httr::oauth_endpoints("google"),
-    credentials = list(access_token = "ACCESS_TOKEN")
+    credentials = list(access_token = "ACCESS_TOKEN"),
+    cache_path = FALSE
   )
   expect_identical(credentials_byo_oauth2(token = token), token)
 })
@@ -32,7 +34,8 @@ test_that("credentials_byo_auth2() extracts a token from a request", {
   token <- httr::Token2.0$new(
     app = httr::oauth_app("x", "y", "z"),
     endpoint = httr::oauth_endpoints("google"),
-    credentials = list(access_token = "ACCESS_TOKEN")
+    credentials = list(access_token = "ACCESS_TOKEN"),
+    cache_path = FALSE
   )
   configured_token <- httr::config(token = token)
   expect_identical(
