49d61c2b9a741f9b1cbeda086ab0cede25c829cd
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-05-02 16:21:49 -0700

    Put `subject` after `...`; document it

--- R/credentials_service_account.R
+++ R/credentials_service_account.R
@@ -4,7 +4,14 @@
 #' @param path JSON identifying the service account, in one of the forms
 #'   supported for the `txt` argument of [jsonlite::fromJSON()] (typically, a
 #'   file path or JSON string).
-#' @param subject TODO: document this properly.
+#' @param subject An optional subject claim. Use for a service account which has
+#'   been granted domain-wide authority by an administrator. Such delegation of
+#'   domain-wide authority means that the service account is permitted to act on
+#'   behalf of users, without their consent. Identify the user to impersonate
+#'   via their email, e.g. `subject = "user@example.com"`.
+#'
+#' @seealso Additional reading on delegation of domain-wide authority:
+#' * <https://developers.google.com/identity/protocols/oauth2/service-account#delegatingauthority>
 #'
 #' @return An [`httr::TokenServiceAccount`][httr::Token-class] or `NULL`.
 #' @family credential functions
@@ -18,8 +25,8 @@
 #' }
 credentials_service_account <- function(scopes = NULL,
                                         path = "",
-                                        subject = NULL,
-                                        ...) {
+                                        ...,
+                                        subject = NULL) {
   ui_line("trying credentials_service_account()")
   info <- jsonlite::fromJSON(path, simplifyVector = FALSE)
   if (!identical(info[["type"]], "service_account")) {
@@ -43,7 +50,7 @@ credentials_service_account <- function(scopes = NULL,
     sub = subject
   )
   if (is.null(token$credentials$access_token) ||
-      !nzchar(token$credentials$access_token)) {
+    !nzchar(token$credentials$access_token)) {
     NULL
   } else {
     ui_line("service account email: ", token_email(token))
