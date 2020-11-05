132d549871ab5d80ae20d21c5b465fdd80ca0f6c
Joe Cheng <joe@rstudio.com>
2020-11-05 10:54:52 -0800

    Update example app

--- inst/shiny-example/app.R
+++ inst/shiny-example/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(googledrive)
 library(gargle)
 library(magrittr)
 
@@ -12,11 +13,19 @@ oauth_scopes = c(
 oauth_app <- gargle_app()
 
 ui <- fluidPage(
-  textOutput("foo")
+  verbatimTextOutput("foo")
 )
 
 server <- function(input, output, session) {
-  output$foo <- renderText({ "hello, world" })
+  output$foo <- renderText({
+    # This is just temporary, we need to make this automatic. Written this way,
+    # it's too easy to get wrong, and also won't work properly with promises
+    drive_auth(token = session$userData$gargle_token)
+    on.exit(drive_deauth())
+
+    listing <- googledrive::drive_find(n_max = 100)
+    paste(collapse = "\n", capture.output(print(listing)))
+  })
 }
 
 shinyApp(ui, server) %>% require_oauth(oauth_app, oauth_scopes, NULL)
