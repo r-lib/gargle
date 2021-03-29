9b5810b43160996ddd8d375840fb74db7592c3e1
Jenny Bryan <jenny.f.bryan@gmail.com>
2021-03-29 16:36:31 -0700

    Start to morph towards something both more general and more Google-specific
    Google-specific? Not worrying about `use_basic_auth`
    
    More general? Stop filtering for specific oauth2.0 errors

--- R/oauth-refresh.R
+++ R/oauth-refresh.R
@@ -5,7 +5,7 @@
 #
 # Refreshes the given token, and returns a new credential with a
 # valid access_token. Based on:
-# https://developers.google.com/accounts/docs/OAuth2InstalledApp#refresh
+# https://developers.google.com/identity/protocols/oauth2/native-app#offline
 refresh_oauth2.0 <- function(endpoint, app, credentials, user_params = NULL,
                              use_basic_auth = FALSE) {
   if (is.null(credentials$refresh_token)) {
@@ -16,6 +16,7 @@ refresh_oauth2.0 <- function(endpoint, app, credentials, user_params = NULL,
   req_params <- list(
     refresh_token = credentials$refresh_token,
     client_id = app$key,
+    client_secret = app$secret,
     grant_type = "refresh_token"
   )
 
@@ -23,15 +24,7 @@ refresh_oauth2.0 <- function(endpoint, app, credentials, user_params = NULL,
     req_params <- utils::modifyList(user_params, req_params)
   }
 
-  if (isTRUE(use_basic_auth)) {
-    response <- httr::POST(refresh_url,
-      body = req_params, encode = "form",
-      httr::authenticate(app$key, app$secret, type = "basic")
-    )
-  } else {
-    req_params$client_secret <- app$secret
-    response <- httr::POST(refresh_url, body = req_params, encode = "form")
-  }
+  response <- httr::POST(refresh_url, body = req_params, encode = "form")
 
   err <- find_oauth2.0_error(response)
   if (!is.null(err)) {
@@ -54,15 +47,6 @@ oauth2.0_error_codes <- c(
   401
 )
 
-oauth2.0_errors <- c(
-  "invalid_request",
-  "invalid_client",
-  "invalid_grant",
-  "unauthorized_client",
-  "unsupported_grant_type",
-  "invalid_scope"
-)
-
 # This implements error checking according to the OAuth2.0
 # specification: https://tools.ietf.org/html/rfc6749#section-5.2
 find_oauth2.0_error <- function(response) {
@@ -71,7 +55,7 @@ find_oauth2.0_error <- function(response) {
   }
 
   content <- httr::content(response)
-  if (!content$error %in% oauth2.0_errors) {
+  if (is.null(content$error)) {
     return(NULL)
   }
 
