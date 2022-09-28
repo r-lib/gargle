2ccace52fe1b139cce6c929666572a2bdc05040b
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-09-28 13:48:01 -0700

    use_lifecycle()

--- R/gargle-package.R
+++ R/gargle-package.R
@@ -7,6 +7,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
 
