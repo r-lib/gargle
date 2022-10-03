8749ceaba896253ac21648f05f6798285205b2a3
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-10-03 13:35:48 -0700

    Update compat files

--- R/compat-types-check.R
+++ R/compat-types-check.R
@@ -8,6 +8,13 @@
 # Changelog
 # =========
 #
+# 2022-09-28:
+# - Removed `what` arguments.
+# - Added `allow_na` and `allow_null` arguments.
+# - Added `allow_decimal` and `allow_infinite` arguments.
+# - Improved errors with absent arguments.
+#
+#
 # 2022-09-16:
 # - Unprefixed usage of rlang functions with `rlang::` to
 #   avoid onLoad issues when called from rlang (#1482).
@@ -19,119 +26,362 @@
 
 check_bool <- function(x,
                        ...,
-                       what = "`TRUE` or `FALSE`",
+                       allow_na = FALSE,
+                       allow_null = FALSE,
                        arg = caller_arg(x),
                        call = caller_env()) {
-  if (!is_bool(x)) {
-    stop_input_type(x, what, ..., arg = arg, call = call)
+  if (!missing(x)) {
+    if (is_bool(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_na && identical(x, NA)) {
+      return(invisible(NULL))
+    }
   }
+
+  stop_input_type(
+    x,
+    c("`TRUE`", "`FALSE`"),
+    ...,
+    allow_na = allow_na,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
 }
 
 check_string <- function(x,
                          ...,
-                         what = "a single string",
+                         allow_empty = FALSE,
+                         allow_na = FALSE,
+                         allow_null = FALSE,
                          arg = caller_arg(x),
                          call = caller_env()) {
-  if (!is_string(x)) {
-    stop_input_type(x, what, ..., arg = arg, call = call)
+  if (!missing(x)) {
+    if (is_string(x)) {
+      if (allow_empty || !is_string(x, "")) {
+        return(invisible(NULL))
+      }
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_na && (identical(x, NA) ||
+                     identical(x, na_chr))) {
+      return(invisible(NULL))
+    }
   }
+
+  stop_input_type(
+    x,
+    "a single string",
+    ...,
+    allow_na = allow_na,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
+
 }
 
-check_number <- function(x,
-                         ...,
-                         what = "a round number",
-                         arg = caller_arg(x),
-                         call = caller_env()) {
-  if (!is_number(x)) {
-    stop_input_type(x, what, ..., arg = arg, call = call)
-  }
+check_number_decimal <- function(x,
+                                 ...,
+                                 allow_infinite = TRUE,
+                                 allow_na = FALSE,
+                                 allow_null = FALSE,
+                                 arg = caller_arg(x),
+                                 call = caller_env()) {
+  .rlang_types_check_number(
+    x,
+    ...,
+    allow_decimal = TRUE,
+    allow_infinite = allow_infinite,
+    allow_na = allow_na,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
 }
 
-is_number <- function(x) {
-  is_integerish(x, n = 1, finite = TRUE)
+check_number_whole <- function(x,
+                               ...,
+                               allow_na = FALSE,
+                               allow_null = FALSE,
+                               arg = caller_arg(x),
+                               call = caller_env()) {
+  .rlang_types_check_number(
+    x,
+    ...,
+    allow_decimal = FALSE,
+    allow_infinite = FALSE,
+    allow_na = allow_na,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
+}
+
+.rlang_types_check_number <- function(x,
+                                      ...,
+                                      allow_decimal = FALSE,
+                                      allow_infinite = FALSE,
+                                      allow_na = FALSE,
+                                      allow_null = FALSE,
+                                      arg = caller_arg(x),
+                                      call = caller_env()) {
+  if (!missing(x)) {
+    is_number <- is_number(
+      x,
+      allow_decimal = allow_decimal,
+      allow_infinite = allow_infinite
+    )
+    if (is_number) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_na && (identical(x, NA) ||
+                     identical(x, na_dbl) ||
+                     identical(x, na_int))) {
+      return(invisible(NULL))
+    }
+  }
+
+  if (allow_decimal) {
+    what <- "a number"
+  } else {
+    what <- "a whole number"
+  }
+
+  stop_input_type(
+    x,
+    what,
+    ...,
+    allow_na = allow_na,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
+}
+
+is_number <- function(x,
+                      allow_decimal = FALSE,
+                      allow_infinite = FALSE) {
+  if (!typeof(x) %in% c("integer", "double")) {
+    return(FALSE)
+  }
+  if (length(x) != 1) {
+    return(FALSE)
+  }
+  if (is.na(x)) {
+    return(FALSE)
+  }
+  if (!allow_decimal && !is_integerish(x)) {
+    return(FALSE)
+  }
+  if (!allow_infinite && is.infinite(x)) {
+    return(FALSE)
+  }
+  TRUE
 }
 
 check_symbol <- function(x,
                          ...,
-                         what = "a symbol",
+                         allow_null = FALSE,
                          arg = caller_arg(x),
                          call = caller_env()) {
-  if (!is_symbol(x)) {
-    stop_input_type(x, what, ..., arg = arg, call = call)
+  if (!missing(x)) {
+    if (is_symbol(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
   }
+
+  stop_input_type(
+    x,
+    "a symbol",
+    ...,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
 }
 
 check_arg <- function(x,
                       ...,
-                      what = "an argument name",
+                      allow_null = FALSE,
                       arg = caller_arg(x),
                       call = caller_env()) {
-  check_symbol(x, ..., what = what, arg = arg, call = call)
+  if (!missing(x)) {
+    if (is_symbol(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
+  }
+
+  stop_input_type(
+    x,
+    "an argument name",
+    ...,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
 }
 
 check_call <- function(x,
                        ...,
-                       what = "a defused call",
+                       allow_null = FALSE,
                        arg = caller_arg(x),
                        call = caller_env()) {
-  if (!is_call(x)) {
-    stop_input_type(x, what, ..., arg = arg, call = call)
+  if (!missing(x)) {
+    if (is_call(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
   }
+
+  stop_input_type(
+    x,
+    "a defused call",
+    ...,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
 }
 
 check_environment <- function(x,
                               ...,
-                              what = "an environment",
+                              allow_null = FALSE,
                               arg = caller_arg(x),
                               call = caller_env()) {
-  if (!is_environment(x)) {
-    stop_input_type(x, what, ..., arg = arg, call = call)
+  if (!missing(x)) {
+    if (is_environment(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
   }
+
+  stop_input_type(
+    x,
+    "an environment",
+    ...,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
 }
 
 check_function <- function(x,
                            ...,
-                           what = "a function",
+                           allow_null = FALSE,
                            arg = caller_arg(x),
                            call = caller_env()) {
-  if (!is_function(x)) {
-    stop_input_type(x, what, ..., arg = arg, call = call)
+  if (!missing(x)) {
+    if (is_function(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
   }
+
+  stop_input_type(
+    x,
+    "a function",
+    ...,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
 }
 
 check_closure <- function(x,
-                           ...,
-                           what = "an R function",
-                           arg = caller_arg(x),
-                           call = caller_env()) {
-  if (!is_closure(x)) {
-    stop_input_type(x, what, ..., arg = arg, call = call)
+                          ...,
+                          allow_null = FALSE,
+                          arg = caller_arg(x),
+                          call = caller_env()) {
+  if (!missing(x)) {
+    if (is_closure(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
   }
+
+  stop_input_type(
+    x,
+    "an R function",
+    ...,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
 }
 
 check_formula <- function(x,
                           ...,
-                          what = "a formula",
+                          allow_null = FALSE,
                           arg = caller_arg(x),
                           call = caller_env()) {
-  if (!is_formula(x)) {
-    stop_input_type(x, what, ..., arg = arg, call = call)
+  if (!missing(x)) {
+    if (is_formula(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
   }
+
+  stop_input_type(
+    x,
+    "a formula",
+    ...,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
 }
 
 
 # Vectors -----------------------------------------------------------------
 
-# TODO: Restrict missing and special values
-
 check_character <- function(x,
                             ...,
-                            what = "a character vector",
+                            allow_null = FALSE,
                             arg = caller_arg(x),
                             call = caller_env()) {
-  if (!is_character(x)) {
-    stop_input_type(x, what, ..., arg = arg, call = call)
+  if (!missing(x)) {
+    if (is_character(x)) {
+      return(invisible(NULL))
+    }
+    if (allow_null && is_null(x)) {
+      return(invisible(NULL))
+    }
   }
+
+  stop_input_type(
+    x,
+    "a character vector",
+    ...,
+    allow_null = allow_null,
+    arg = arg,
+    call = call
+  )
 }
 
 # nocov end
