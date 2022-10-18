9240c44c72e3317131d28349e8d562bb92442677
Jenny Bryan <jenny.f.bryan@gmail.com>
2022-10-18 12:47:23 -0700

    We use rlang::is_interactive() in gargle
    This came from httr and probably predates rlang::is_interactive().

--- R/oauth-init.R
+++ R/oauth-init.R
@@ -157,9 +157,6 @@ check_scope <- function(x) {
   paste(x, collapse = " ")
 }
 
-# Wrap base::interactive in a non-primitive function so that the call can be mocked for testing
-is_interactive <- function() interactive()
-
 check_oob <- function(use_oob, oob_value = NULL) {
   if (!is.logical(use_oob) || length(use_oob) != 1) {
     stop("`use_oob` must be a length-1 logical vector", call. = FALSE)
