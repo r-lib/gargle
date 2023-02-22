393765b6f0cad4edfbf83bbec8bff283d0cddcb3
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-02-22 09:47:18 -0800

    Use dev httr instead of patch

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,4 @@
 .onLoad <- function(lib, pkg) { # nocov start
-  # we need a way to tell httr that a Google Colab session is interactive
-  utils::assignInNamespace("is_interactive", rlang::is_interactive, ns = "httr")
-
   cred_funs_set_default()
   invisible()
 } # nocov end
